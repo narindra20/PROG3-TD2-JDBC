7ed3e6e8a9177ef55267a0170b9e630c86066e18
narindra20 <hei.narindra@gmail.com>
2025-12-15 11:10:30 +0300

    Creation du database Connection

---
+++ src/main/java/database/DBConnection.java
@@ -0,0 +1,16 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/mini_football_db";
+    private static final String USER = "mini_football_db_manager";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
